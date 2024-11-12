40947e1a988d2ee485cca2777ca07d5c4ab5532a
ocnwaokoro <chibuikem.nwaokoro@gmail.com>
2024-11-12 00:42:36 -0500

    Fix linting errors

--- machine-learning-client/train_mnist_cnn.py
+++ machine-learning-client/train_mnist_cnn.py
@@ -1,40 +1,9 @@
 """ Module for training MNIST CNN model"""
 
 import torch
-from torch import nn
 from torch import optim
-import torch.nn.functional as F
 from torchvision import datasets, transforms
-
-
-class CNNModel(nn.Module):
-    def __init__(self):
-        super(CNNModel, self).__init__()
-        # 1 input channel (grayscale), 32 output channels
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(128 * 3 * 3, 128)
-        self.fc2 = nn.Linear(128, 10)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.dropout = nn.Dropout(0.25)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = x.view(-1, 128 * 3 * 3)  # flatten
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
-
-
-class InvertGrayscale:
-    """Class used to transform data and invert grayscale"""
-
-    def __call__(self, tensor):
-        return 255.0 - tensor
+from model import *
 
 
 def train(model, train_loader, optimizer, criterion, device):
