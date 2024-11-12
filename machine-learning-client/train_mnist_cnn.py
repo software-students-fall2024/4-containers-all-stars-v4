4982032d5fa8a6391306de17b1c0b4b8d49b55a2
ocnwaokoro <chibuikem.nwaokoro@gmail.com>
2024-11-12 00:35:51 -0500

    Update train_mnist_cnn.py

--- machine-learning-client/train_mnist_cnn.py
+++ machine-learning-client/train_mnist_cnn.py
@@ -1,6 +1,8 @@
+""" Module for training MNIST CNN model"""
+
 import torch
-import torch.nn as nn
-import torch.optim as optim
+from torch import nn
+from torch import optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 
@@ -28,17 +30,69 @@ class CNNModel(nn.Module):
         return x
 
 
-model = CNNModel()
-
-# ensure that images have black digits with white background
-
-
 class InvertGrayscale:
+    """Class used to transform data and invert grayscale"""
+
     def __call__(self, tensor):
         return 255.0 - tensor
 
 
+def train(model, train_loader, optimizer, criterion, device):
+    """Method for training model"""
+    model.train()
+    running_loss = 0.0
+    for data, target in train_loader:
+        data, target = data.to(device), target.to(device)
+
+        # zero gradients
+        optimizer.zero_grad()
+
+        # forward pass
+        output = model(data)
+
+        # compute loss
+        loss = criterion(output, target)
+
+        # backward pass & optimization
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    avg_loss = running_loss / len(train_loader)
+    print(f"Train Loss: {avg_loss:.4f}")
+    return avg_loss
+
+
+def test(model, test_loader, criterion, device):
+    """Method for testing model"""
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+
+            # forward pass
+            output = model(data)
+
+            # compute loss
+            test_loss += criterion(output, target).item()
+
+            # get predictions
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader)
+    accuracy = 100.0 * correct / len(test_loader.dataset)
+    print(f"Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%")
+    return test_loss, accuracy
+
+
 def main():
+    """Main method to train and save model"""
+    model = CNNModel()
+
     transform = transforms.Compose([transforms.ToTensor(), InvertGrayscale()])
 
     dataset = datasets.MNIST(
@@ -48,28 +102,28 @@ def main():
         dataset, batch_size=64, shuffle=False, num_workers=0
     )
 
-    MEAN = 0.0
-    STD = 0.0
+    mean = 0.0
+    std = 0.0
     total_images = 0
 
     for images, _ in loader:
         batch_samples = images.size(0)  # batch size
         # flatten the spatial dimensions
         images = images.view(batch_samples, images.size(1), -1)
-        MEAN += images.mean(2).sum(0)
-        STD += images.std(2).sum(0)
+        mean += images.mean(2).sum(0)
+        std += images.std(2).sum(0)
         total_images += batch_samples
 
-    MEAN /= total_images
-    STD /= total_images
+    mean /= total_images
+    std /= total_images
 
-    CHAR_COUNT = 100
+    charCount = 100
 
-    print("\n" + "=" * CHAR_COUNT + "\n")
+    print("\n" + "=" * charCount + "\n")
     print("DEBUG: Dataset Characteristics")
-    print(f" * Mean: {float(MEAN):.4f}")
-    print(f" * Std: {float(STD):.4f}")
-    print("\n" + "=" * CHAR_COUNT + "\n")
+    print(f" * mean: {float(mean):.4f}")
+    print(f" * std: {float(std):.4f}")
+    print("\n" + "=" * charCount + "\n")
 
     train_transform = transforms.Compose(
         [
@@ -80,7 +134,7 @@ def main():
             transforms.RandomResizedCrop(28, scale=(0.9, 1.1)),  # randomly scale images
             transforms.ToTensor(),
             InvertGrayscale(),
-            transforms.Normalize((MEAN,), (STD,)),
+            transforms.Normalize((mean,), (std,)),
         ]
     )
 
@@ -88,7 +142,7 @@ def main():
         [
             transforms.ToTensor(),
             InvertGrayscale(),
-            transforms.Normalize((MEAN,), (STD,)),
+            transforms.Normalize((mean,), (std,)),
         ]
     )
 
@@ -106,54 +160,6 @@ def main():
         test_dataset, batch_size=1000, shuffle=False
     )
 
-    def train(model, train_loader, optimizer, criterion, device):
-        model.train()
-        running_loss = 0.0
-        for data, target in train_loader:
-            data, target = data.to(device), target.to(device)
-
-            # zero gradients
-            optimizer.zero_grad()
-
-            # forward pass
-            output = model(data)
-
-            # compute loss
-            loss = criterion(output, target)
-
-            # backward pass & optimization
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-
-        avg_loss = running_loss / len(train_loader)
-        print(f"Train Loss: {avg_loss:.4f}")
-        return avg_loss
-
-    def test(model, test_loader, criterion, device):
-        model.eval()
-        test_loss = 0
-        correct = 0
-        with torch.no_grad():
-            for data, target in test_loader:
-                data, target = data.to(device), target.to(device)
-
-                # forward pass
-                output = model(data)
-
-                # compute loss
-                test_loss += criterion(output, target).item()
-
-                # get predictions
-                pred = output.argmax(dim=1, keepdim=True)
-                correct += pred.eq(target.view_as(pred)).sum().item()
-
-        test_loss /= len(test_loader)
-        accuracy = 100.0 * correct / len(test_loader.dataset)
-        print(f"Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%")
-        return test_loss, accuracy
-
     # device specifications
     device = torch.device("mps" if torch.mps.is_available() else "cpu")
 
@@ -170,14 +176,16 @@ def main():
     # train test loop
     for epoch in range(1, epochs + 1):
         print(f"Epoch {epoch}/{epochs}")
-        train_loss = train(model, train_loader, optimizer, criterion, device)
-        test_loss, accuracy = test(model, test_loader, criterion, device)
-    print("\n" + "=" * CHAR_COUNT + "\n")
+        # train_loss = train(model, train_loader, optimizer, criterion, device)
+        # test_loss, accuracy = test(model, test_loader, criterion, device)
+        train(model, train_loader, optimizer, criterion, device)
+        test(model, test_loader, criterion, device)
+    print("\n" + "=" * charCount + "\n")
 
     # save model
     torch.save(model.state_dict(), "mnist-cnn.pth")
     print("DEBUG: \x1B[1mModel Saved!\x1B[0m")
-    print("\n" + "=" * CHAR_COUNT)
+    print("\n" + "=" * charCount)
 
 
 if __name__ == "__main__":
