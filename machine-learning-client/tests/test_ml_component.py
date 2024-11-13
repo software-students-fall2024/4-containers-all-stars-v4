5f11d29bedc27da0b87a3ef40283ac15b0eb9019
ianpompliano <inp9194@nyu.edu>
2024-11-12 20:16:19 -0500

    added test template

---
+++ machine-learning-client/tests/test_ml_component.py
@@ -0,0 +1,17 @@
+"""Used to test machine learning component."""
+import pytest
+
+class Tests:
+    """Test functions"""
+    def test_sanity_check(self):
+        """
+        Test debugging... making sure that we can run a simple test that always passes.
+        Note the use of the example_fixture in the parameter list - 
+        any setup and teardown in that fixture will be run before 
+        and after this test function executes.
+        """
+        expected = True
+        actual = True
+        assert actual == expected, "Expected True to be equal to True!"
+        
+    #write the rest of the tests here.
